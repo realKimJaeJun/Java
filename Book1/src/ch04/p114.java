1bf7dab9dd81ce3e11aa47a075fc96d5f0b91ca1
realKimJaeJUN <nagandr1113@gmail.com>
2022-08-23 19:52:29 +0900

    Book1 예제 추가

---
+++ Book1/src/ch04/p114.java
@@ -0,0 +1,19 @@
+package ch04;
+
+public class p114 {
+	public static void main(String[] args) {
+		int i;
+		int sum = 0;
+		for(i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println("1부터 10까지의 합은 "+ sum + "입니다.");
+		
+		int num = 0;
+		int m;
+		for(m = 1; m < 11; m++) {
+			num = m;
+			System.out.println("안녕하세요"+num);
+		}
+	}
+}
