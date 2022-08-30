cf5c2abb0742f3634852033ead3f6ed8e78282c9
realKimJaeJUN <nagandr1113@gmail.com>
2022-08-30 17:53:32 +0900

    Book1 예제 수정

--- Book1/src/ch05/p132.java
+++ Book1/src/ch05/p132.java
@@ -1,12 +1,16 @@
 package ch05;
-
+/*
+ * 날짜: 2022/08/30
+ * 이름: 김재준
+ * 내용: 학생 이름과 주소 출력하는 메서드 만들기
+ */
 public class p132 {
 	int studentId;		// 학번
 	String studentName;	// 학생 이름
 	int grade;			// 학년
 	String address;		// 사는 곳
 	
-	public void showStudentInfo() {
+	public void showStudentInfo( ) {
 		System.out.println(studentName + "," + address); // 이름, 주소 출력
 	}
 }
