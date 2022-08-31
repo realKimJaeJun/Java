5845e487c6788a2e41c350bc1be3330d55877ff6
realKimJaeJUN <nagandr1113@gmail.com>
2022-08-31 19:51:19 +0900

    교재 예제, ch05 예제 추가

---
+++ Ch05/src/sub6/Car.java
@@ -0,0 +1,33 @@
+package sub6;
+
+public class Car {
+	// 속성 field
+	protected String name;
+	protected String color;
+	protected int speed;
+	
+	// 생성자
+	public Car(String name, String color, int speed) {
+		this.name = name;
+		this.color = color;
+		this.speed = speed;
+
+	}
+	
+	// 기능
+	public void speedUp(int speed) {
+		this.speed += speed;
+	}
+	public void speedDown(int speed) {
+		this.speed -= speed;
+	}
+	public void show() {
+		System.out.println("차량명 : "+name);
+		System.out.println("차량색 : "+color);
+	    System.out.println("현재속도 : "+speed);
+
+	    
+	}
+	
+	
+}
