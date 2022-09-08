9b8c5413bcfa088c048cd401727c671575a64af5
realKimJaeJUN <nagandr1113@gmail.com>
2022-09-08 19:51:24 +0900

    Book1 예제 추가

---
+++ Book1/src/ch05/p144.java
@@ -0,0 +1,21 @@
+package ch05;
+
+public class p144 {
+
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public static void main(String[] args) {
+		p144 studentAhn = new p144(); //Student 클래스 생성
+		studentAhn.studentName = "안연수";
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+	}
+}
