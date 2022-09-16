d36b5761f67867efa3ea0a98d08dd70dd747b55a
realKimJaeJUN <nagandr1113@gmail.com>
2022-09-16 15:01:55 +0900

    예제 추가

--- Backjoon/src/step03/_11.java
+++ Backjoon/src/step03/_11.java
@@ -1,5 +1,21 @@
 package step03;
 
-public class _11 {
+import java.util.Scanner;
 
+public class _11 {
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(a==0 && b==0) {
+				sc.close();
+				break;
+			}
+			System.out.println(a+b);
+		}
+	}
 }
--- Backjoon/src/step03/_12.java
+++ Backjoon/src/step03/_12.java
@@ -1,5 +1,18 @@
 package step03;
 
-public class _12 {
+import java.util.Scanner;
 
+public class _12 {
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+		sc.close();
+	}
 }
