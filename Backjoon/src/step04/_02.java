b209fef1768a21d69b02d01e7d0b07e44f3d7aec
realKimJaeJUN <nagandr1113@gmail.com>
2022-09-20 15:00:28 +0900

    백준 예제추가

--- Backjoon/src/step04/_02.java
+++ Backjoon/src/step04/_02.java
@@ -1,5 +1,30 @@
 package step04;
 
-public class _02 {
+import java.util.Scanner;
 
+/*
+ * 날짜: 2022/09/20
+ * 이름: 김재준
+ * 내용: 백준 4단계 2번 문제. 최댓값
+ */
+public class _02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = {sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()};
+		sc.close();
+		
+		int count = 0;
+		int max = 0;
+		int index = 0;
+		
+		for(int value : arr) {
+			count++;
+			
+			if(value > max) {
+				max = value;
+				index = count;
+			}
+		}
+		System.out.println(max + "\n" + index);
+	}
 }
